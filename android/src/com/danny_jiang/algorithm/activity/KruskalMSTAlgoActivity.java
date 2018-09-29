4adeab5622f982119ac5b33364ef08af8bcefb59
Danny.Jiang <ext.danny.jiang@ef.com>
2018-09-29 11:38:50 +0800

    add BubbleVisualizerFragment

--- android/src/com/danny_jiang/algorithm/activity/KruskalMSTAlgoActivity.java
+++ android/src/com/danny_jiang/algorithm/activity/KruskalMSTAlgoActivity.java
@@ -1,6 +1,8 @@
 package com.danny_jiang.algorithm.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
@@ -9,20 +11,50 @@ import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
 import com.danny_jiang.algorithm.R;
 import com.danny_jiang.algorithm.adapter.KruskalMSTAdapter;
 import com.danny_jiang.algorithm.fragment.BubbleSortFragment;
+import com.danny_jiang.algorithm.fragment.TestFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KruskalMSTAlgoActivity extends FragmentActivity implements AndroidFragmentApplication.Callbacks {
+
+    private TabLayout tabLayout;
+    private List<Fragment> fragmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //initialize(new KruskalMSTAdapter());
         setContentView(R.layout.common_activity_layout);
 
+        initFragments();
 
-        BubbleSortFragment categorizationFragment = new BubbleSortFragment();
-        FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
-        trans.replace(R.id.visualizer, categorizationFragment);
-        trans.commit();
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("TAB1"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB2"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB3"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment fragment = fragmentList.get(position);
+                FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+                trans.replace(R.id.algo_content, fragment);
+                trans.commit();
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    private void initFragments() {
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new TestFragment());
+        fragmentList.add(new TestFragment());
+        fragmentList.add(new BubbleSortFragment());
     }
 
     @Override
--- android/src/com/danny_jiang/algorithm/fragment/BubbleSortFragment.java
+++ android/src/com/danny_jiang/algorithm/fragment/BubbleSortFragment.java
@@ -3,21 +3,28 @@ package com.danny_jiang.algorithm.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
-import com.danny_jiang.algorithm.adapter.BubbleSortAdapter;
+import com.danny_jiang.algorithm.R;
 
-public class BubbleSortFragment extends AndroidFragmentApplication {
+public class BubbleSortFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        config.numSamples = 4;
-        return initializeForView(new BubbleSortAdapter(), config);
+        return inflater.inflate(R.layout.bubble_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        BubbleSortVisualizerFragment categorizationFragment = new BubbleSortVisualizerFragment();
+        FragmentTransaction trans = getChildFragmentManager().beginTransaction();
+        trans.replace(R.id.visualizer, categorizationFragment);
+        trans.commit();
     }
 }
