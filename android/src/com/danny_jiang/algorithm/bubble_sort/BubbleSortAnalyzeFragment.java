3952d84e176490973bf8da5b5b3c959bc5eaf249
Danny.Jiang <ext.danny.jiang@ef.com>
2018-10-03 22:57:21 +0800

    add WebView

--- android/src/com/danny_jiang/algorithm/bubble_sort/BubbleSortAnalyzeFragment.java
+++ android/src/com/danny_jiang/algorithm/bubble_sort/BubbleSortAnalyzeFragment.java
@@ -7,14 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.danny_jiang.algorithm.R;
 
 public class BubbleSortAnalyzeFragment extends Fragment {
 
+    private WebView webView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.bubble_sort_analyze_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        webView = ((WebView) view.findViewById(R.id.bubble_sort_analyze_webview));
+        webView.loadUrl("https://blog.csdn.net/zxm317122667/article/details/82928899");
     }
 }
