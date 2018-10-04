2df625b11bc3697d9ccc9142d3f6599d835cf2c5
Danny.Jiang <ext.danny.jiang@ef.com>
2018-10-04 11:42:30 +0800

    add Heap Sort pachage

---
+++ android/src/com/danny_jiang/algorithm/heap_sort/HeapSortAdapter.java
@@ -0,0 +1,20 @@
+package com.danny_jiang.algorithm.heap_sort;
+
+import com.danny_jiang.algorithm.common.AlgorithmAdapter;
+
+public class HeapSortAdapter extends AlgorithmAdapter{
+    @Override
+    protected void nextStep() {
+
+    }
+
+    @Override
+    protected void inflateStage() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ android/src/com/danny_jiang/algorithm/heap_sort/HeapSortVisualizerFragment.java
@@ -0,0 +1,24 @@
+package com.danny_jiang.algorithm.heap_sort;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
+
+public class HeapSortVisualizerFragment extends AndroidFragmentApplication {
+    private HeapSortAdapter heapSortAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        heapSortAdapter = new HeapSortAdapter();
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.numSamples = 4;
+        return initializeForView(heapSortAdapter, config);
+    }
+}
