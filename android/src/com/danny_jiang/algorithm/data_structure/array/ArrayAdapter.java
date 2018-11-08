883b19d69b6baf4781c085e7d4a062ad4f4239ca
Danny.Jiang <ext.danny.jiang@ef.com>
2018-11-08 22:52:31 +0800

    add array font

--- android/src/com/danny_jiang/algorithm/data_structure/array/ArrayAdapter.java
+++ android/src/com/danny_jiang/algorithm/data_structure/array/ArrayAdapter.java
@@ -2,12 +2,15 @@ package com.danny_jiang.algorithm.data_structure.array;
 
 import android.os.Message;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.danny_jiang.algorithm.common.AlgorithmAdapter;
 
 import java.util.ArrayList;
@@ -23,6 +26,8 @@ public class ArrayAdapter extends AlgorithmAdapter {
     private List<ArrayElement> elementList = new ArrayList<>();
     private ArrayElement sixElement;
     private ArrayElement emptyElement;
+
+    private Label stepDescription;
 
     @Override
     protected void inflateStage() {
@@ -47,6 +52,20 @@ public class ArrayAdapter extends AlgorithmAdapter {
                 lastElement.getY());
         stage.addActor(emptyElement);
         emptyElement.setVisible(false);
+
+        BitmapFont bitmapFont = new BitmapFont(
+                Gdx.files.internal("data_structure/Array/array.fnt"));
+        Label.LabelStyle style = new Label.LabelStyle();
+        style.font = bitmapFont;
+        style.fontColor = Color.valueOf("#696969");
+        stepDescription = new Label("", style);
+        stepDescription.setAlignment(Align.topLeft);
+        stepDescription.setSize(stage.getWidth(), stage.getHeight() / 2 - next.getHeight() - 100);
+        stepDescription.setFontScale(2f);
+        stepDescription.setPosition(15, next.getHeight() + 10);
+        stage.addActor(stepDescription);
+        stepDescription.setText("数组是一种线性表结构\n它用一组连续的" +
+                "内存空间\n来存储具有相同类型的数据");
     }
 
     @Override
@@ -54,44 +73,95 @@ public class ArrayAdapter extends AlgorithmAdapter {
     }
 
     @Override
-    protected void animation(Message msg) {
-        switch (msg.what) {
-            case INSERT:
-                ArrayElement element1 = elementList.get(3);
-                MoveByAction moveByAction1 = Actions.moveBy(190, 0);
-                moveByAction1.setDuration(0.5f);
-                element1.addAction(moveByAction1);
-
-                ArrayElement element2 = elementList.get(2);
-                float originalX = element2.getX();
-                float originalY = element2.getY();
-                MoveByAction moveByAction2 = Actions.moveBy(190, 0);
-                moveByAction2.setDuration(0.5f);
-                element2.addAction(Actions.sequence(
-                        Actions.delay(0.5f), moveByAction2));
-
-                MoveToAction moveToAction = Actions.moveTo(originalX, originalY);
-                moveToAction.setDuration(0.5f);
-                sixElement.addAction(Actions.sequence(Actions.delay(1),
-                        moveToAction, Actions.run(() -> emptyElement.setVisible(false))));
-                break;
-            case DELETE:
-                break;
-            case VISIBLE_SIX:
-                emptyElement.setVisible(true);
-                sixElement.setVisible(true);
-                break;
-        }
-    }
-
-    @Override
     protected void algorithm() {
         await();
 
-        await((BeforeWaitCallback)() -> sDecodingThreadHandler.sendMessage(
+        await((BeforeWaitCallback) () -> sDecodingThreadHandler.sendMessage(
                 sDecodingThreadHandler.obtainMessage(VISIBLE_SIX)));
 
-        await((BeforeWaitCallback)() -> sDecodingThreadHandler.sendMessage(
+        await((BeforeWaitCallback) () -> sDecodingThreadHandler.sendMessage(
                 sDecodingThreadHandler.obtainMessage(INSERT)));
+
+        await((BeforeWaitCallback) () -> sDecodingThreadHandler.sendMessage(
+                sDecodingThreadHandler.obtainMessage(DELETE)));
     }
+
+    @Override
+    protected void animation(Message msg) {
+        switch (msg.what) {
+            case INSERT:
+                insertOperation();
+            break;
+            case DELETE:
+                deleteOperation();
+            break;
+            case VISIBLE_SIX: {
+                emptyElement.setVisible(true);
+                sixElement.setVisible(true);
+            }
+            break;
+        }
+    }
+
+    private void insertOperation() {
+        ArrayElement element1 = elementList.get(3);
+        MoveByAction moveByAction1 = Actions.moveBy(190, 0);
+        moveByAction1.setDuration(0.5f);
+        element1.addAction(moveByAction1);
+
+        ArrayElement element2 = elementList.get(2);
+        float originalX = element2.getX();
+        float originalY = element2.getY();
+        MoveByAction moveByAction2 = Actions.moveBy(190, 0);
+        moveByAction2.setDuration(0.5f);
+        element2.addAction(Actions.sequence(
+                Actions.delay(0.5f), moveByAction2));
+
+        MoveToAction moveToAction = Actions.moveTo(originalX, originalY);
+        moveToAction.setDuration(0.5f);
+        sixElement.addAction(Actions.sequence(Actions.delay(1),
+                moveToAction, Actions.run(() -> emptyElement.setVisible(false))));
+    }
+
+    private void deleteOperation() {
+        final float toX = 430;
+        final float toY = stage.getHeight() * 3 / 4 - 200;
+        final float originalX = sixElement.getX() - 10;
+        final float originalY = sixElement.getY();
+
+        ArrayElement element1 = elementList.get(3);
+
+        ArrayElement element2 = elementList.get(2);
+
+        RunnableAction run1 = Actions.run(() -> {
+            MoveToAction moveToAction = Actions.moveTo(toX, toY);
+            moveToAction.setDuration(0.5f);
+            sixElement.addAction(moveToAction);
+        });
+
+        RunnableAction emptyAction = Actions.run(() -> {
+            emptyElement.setPosition(originalX, originalY);
+            emptyElement.setVisible(true);
+        });
+
+        RunnableAction run2 = Actions.run(() -> {
+            MoveByAction moveByAction = Actions.moveBy(-190, 0);
+            moveByAction.setDuration(0.5f);
+            element2.addAction(moveByAction);
+        });
+
+        RunnableAction run3 = Actions.run(() -> {
+            MoveByAction moveByAction = Actions.moveBy(-190, 0);
+            moveByAction.setDuration(0.5f);
+            element1.addAction(moveByAction);
+        });
+
+        RunnableAction run4 = Actions.run(() ->
+                emptyElement.setVisible(false));
+
+        stage.addAction(Actions.sequence(run1, Actions.delay(0.5f), emptyAction,
+                run2, Actions.delay(0.5f), run3,
+                Actions.delay(0.5f), run4));
+    }
+
 }
