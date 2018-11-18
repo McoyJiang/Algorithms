d9a6746903ce2384444757415bfcb02d37607987
Danny.Jiang <ext.danny.jiang@ef.com>
2018-11-18 15:05:46 +0800

    add linklist url

--- android/src/com/danny_jiang/algorithm/data_structure/array/ArrayActivity.java
+++ android/src/com/danny_jiang/algorithm/data_structure/array/ArrayActivity.java
@@ -1,10 +1,6 @@
 package com.danny_jiang.algorithm.data_structure.array;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-
 import com.danny_jiang.algorithm.AlgorithmActivity;
-import com.danny_jiang.algorithm.R;
 
 public class ArrayActivity extends AlgorithmActivity {
     @Override
@@ -12,10 +8,5 @@ public class ArrayActivity extends AlgorithmActivity {
         fragmentList.add(new ArrayTutorialFragment());
         fragmentList.add(new ArrayVisualizerFragment());
         fragmentList.add(new ArrayQuizFragment());
-
-//        Fragment fragment = fragmentList.get(0);
-//        FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
-//        trans.replace(R.id.algo_content, fragment);
-//        trans.commit();
     }
 }
