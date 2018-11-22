9ef3abe92364f301a80a41b312034b9e461edce9
Danny.Jiang <ext.danny.jiang@ef.com>
2018-11-22 22:17:02 +0800

    add description

--- android/src/com/danny_jiang/algorithm/data_structure/linkedlist/LinkListAdapter.java
+++ android/src/com/danny_jiang/algorithm/data_structure/linkedlist/LinkListAdapter.java
@@ -4,9 +4,12 @@ import android.os.Message;
 import android.util.Log;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.danny_jiang.algorithm.common.AlgorithmAdapter;
 
 import java.util.ArrayList;
@@ -19,21 +22,31 @@ class LinkListAdapter extends AlgorithmAdapter {
 
     private int[] arr = new int[]{3, 8, 10, 9, 15, 35};
 
+    private Label stepDescription;
     private List<NodeActor> addedActorList = new ArrayList<>();
 
     @Override
     protected void inflateStage() {
-        try {
-            NodeActor currentNodeActor = new NodeActor(8);
-            currentNodeActor.setSize(150, 150);
-            currentNodeActor.setPosition(
-                    stage.getWidth() / 2 - currentNodeActor.getWidth() / 2,
-                    stage.getHeight() * 3 / 4 - currentNodeActor.getHeight() / 2);
-            stage.addActor(currentNodeActor);
-            addedActorList.add(0, currentNodeActor);
-        } catch (Exception e) {
-            Log.e("BBB", "e here is " + e.getMessage());
-        }
+        BitmapFont bitmapFont = new BitmapFont(Gdx.files.internal(
+                "data_structure/LinkedList/linkedlist.fnt"));
+        bitmapFont.getData().scale(1.5f);
+        Label.LabelStyle style = new Label.LabelStyle();
+        style.font = bitmapFont;
+        style.fontColor = new Color(1, 0, 0, 1);
+        stepDescription = new Label("", style);
+        stepDescription.setText("链表是一种线性表,它通过指针\n将一组零散的数据链接起来.");
+        stepDescription.setSize(500, 350);
+        stepDescription.setFontScale(2f);
+        stepDescription.setPosition(30, stage.getHeight() / 3 - 100);
+        stage.addActor(stepDescription);
+
+        NodeActor currentNodeActor = new NodeActor(8);
+        currentNodeActor.setSize(150, 150);
+        currentNodeActor.setPosition(
+                stage.getWidth() / 2 - currentNodeActor.getWidth() / 2,
+                stage.getHeight() * 3 / 4 - currentNodeActor.getHeight() / 2);
+        stage.addActor(currentNodeActor);
+        addedActorList.add(0, currentNodeActor);
     }
 
     @Override
@@ -45,19 +58,20 @@ class LinkListAdapter extends AlgorithmAdapter {
     protected void animation(Message msg) {
         switch (msg.what) {
             case ADD_TO_HEAD:
-                addNodeToHead();
+                addNodeToHead(msg.arg1);
                 break;
             case ADD_TO_TAIL:
-                addNodeToTail();
+                addNodeToTail(msg.arg1);
                 break;
         }
     }
 
-    private void addNodeToHead() {
+    private void addNodeToHead(final int number) {
         Gdx.app.postRunnable(() -> {
+            stepDescription.setText("单链表：addFirst");
             float x = addedActorList.get(0).getX() + 75;
             float y = addedActorList.get(0).getY();
-            NodeActor nodeActor = new NodeActor(4);
+            NodeActor nodeActor = new NodeActor(number);
             nodeActor.setSize(150, 150);
             nodeActor.setPosition(stage.getWidth() / 2 - nodeActor.getWidth() / 2,
                     stage.getHeight() - nodeActor.getHeight() - 10);
@@ -77,11 +91,12 @@ class LinkListAdapter extends AlgorithmAdapter {
         });
     }
 
-    private void addNodeToTail() {
+    private void addNodeToTail(final int number) {
         Gdx.app.postRunnable(() -> {
+            stepDescription.setText("单链表：addLast");
             float x = addedActorList.get(addedActorList.size() - 1).getX() + 75;
             float y = addedActorList.get(0).getY();
-            NodeActor nodeActor = new NodeActor(4);
+            NodeActor nodeActor = new NodeActor(number);
             nodeActor.setSize(150, 150);
             nodeActor.setPosition(stage.getWidth() / 2 - nodeActor.getWidth() / 2,
                     stage.getHeight() - nodeActor.getHeight() - 10);
@@ -116,15 +131,15 @@ class LinkListAdapter extends AlgorithmAdapter {
         });
         await((BeforeWaitCallback)() -> {
             sDecodingThreadHandler.sendMessage(
-                    sDecodingThreadHandler.obtainMessage(ADD_TO_HEAD, 10, -1));
+                    sDecodingThreadHandler.obtainMessage(ADD_TO_HEAD, 3, -1));
         });
         await((BeforeWaitCallback)() -> {
             sDecodingThreadHandler.sendMessage(
-                    sDecodingThreadHandler.obtainMessage(ADD_TO_TAIL, 10, -1));
+                    sDecodingThreadHandler.obtainMessage(ADD_TO_TAIL, 36, -1));
         });
         await((BeforeWaitCallback)() -> {
             sDecodingThreadHandler.sendMessage(
-                    sDecodingThreadHandler.obtainMessage(ADD_TO_TAIL, 10, -1));
+                    sDecodingThreadHandler.obtainMessage(ADD_TO_TAIL, 15, -1));
         });
     }
 }
