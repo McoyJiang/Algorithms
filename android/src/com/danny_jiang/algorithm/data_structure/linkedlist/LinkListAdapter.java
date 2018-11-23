14bc749ea1fdf9d081846a657655e6dee3664ddf
Danny.Jiang <ext.danny.jiang@ef.com>
2018-11-23 10:07:12 +0800

    finish linked list

--- android/src/com/danny_jiang/algorithm/data_structure/linkedlist/LinkListAdapter.java
+++ android/src/com/danny_jiang/algorithm/data_structure/linkedlist/LinkListAdapter.java
@@ -17,6 +17,8 @@ class LinkListAdapter extends AlgorithmAdapter {
 
     private static final int ADD_TO_HEAD = 1;
     private static final int ADD_TO_TAIL = 2;
+    private static final int ADD_TO_N = 3;
+    private static final int DONE = 4;
 
     private int[] arr = new int[]{3, 8, 10, 9, 15, 35};
 
@@ -29,7 +31,7 @@ class LinkListAdapter extends AlgorithmAdapter {
                 "data_structure/LinkedList/linkedlist.fnt"));
         Label.LabelStyle style = new Label.LabelStyle();
         style.font = bitmapFont;
-        style.fontColor = new Color(1, 0, 0, 1);
+        style.fontColor = Color.valueOf("#4A4A4A");
         stepDescription = new Label("", style);
         stepDescription.setText("链表是一种线性表,它通过指针\n将一组零散的数据链接起来.");
         stepDescription.setSize(500, 350);
@@ -60,12 +62,58 @@ class LinkListAdapter extends AlgorithmAdapter {
             case ADD_TO_TAIL:
                 addNodeToTail(msg.arg1);
                 break;
+            case ADD_TO_N:
+                addNodeToN(msg.arg1);
+                break;
+            case DONE:
+                Gdx.app.postRunnable(() -> {
+                    stepDescription.setText("DONE");
+                    next.setText("Done");
+                });
+                break;
         }
+    }
+
+    private void addNodeToN(int number) {
+        Gdx.app.postRunnable(() -> {
+            stepDescription.setText("单链表:add(int n, int value)\n" +
+                    "将元素插入到链表第n个位置\n" +
+                    "时间复杂度为：O(n)");
+            float x = addedActorList.get(2).getX() + 75;
+            float y = addedActorList.get(2).getY();
+
+            NodeActor nodeActor = new NodeActor(number);
+            nodeActor.setSize(150, 150);
+            nodeActor.setPosition(stage.getWidth() / 2 - nodeActor.getWidth() / 2,
+                    stage.getHeight() - nodeActor.getHeight() - 10);
+            nodeActor.setTail(false);
+            stage.addActor(nodeActor);
+
+            MoveToAction moveToAction = Actions.moveTo(x - 160, y);
+            moveToAction.setDuration(1);
+            nodeActor.addAction(moveToAction);
+            nodeActor.addNodeToHead(x, y);
+            for (int i = 0; i < addedActorList.size(); i++) {
+                NodeActor actor = addedActorList.get(i);
+                if (i <= 1) {
+                    MoveByAction moveByAction = Actions.moveBy(-90, 0);
+                    moveByAction.setDuration(1);
+                    actor.addAction(moveByAction);
+                } else {
+                    MoveByAction moveByAction = Actions.moveBy(110, 0);
+                    moveByAction.setDuration(1);
+                    actor.addAction(moveByAction);
+                }
+            }
+            addedActorList.add(nodeActor);
+        });
     }
 
     private void addNodeToHead(final int number) {
         Gdx.app.postRunnable(() -> {
-            stepDescription.setText("单链表：addFirst");
+            stepDescription.setText("单链表:addFirst\n" +
+                    "将元素插入到链表头部\n" +
+                    "时间复杂度为：O(1)");
             float x = addedActorList.get(0).getX() + 75;
             float y = addedActorList.get(0).getY();
             NodeActor nodeActor = new NodeActor(number);
@@ -90,8 +138,10 @@ class LinkListAdapter extends AlgorithmAdapter {
 
     private void addNodeToTail(final int number) {
         Gdx.app.postRunnable(() -> {
-            stepDescription.setText("单链表：addLast");
-            float x = addedActorList.get(addedActorList.size() - 1).getX() + 75;
+            stepDescription.setText("单链表:addLast\n" +
+                    "将元素插入到链表尾部\n" +
+                    "时间复杂度为：O(1)");
+            float x = addedActorList.get(addedActorList.size() - 1).getX() + 65;
             float y = addedActorList.get(0).getY();
             NodeActor nodeActor = new NodeActor(number);
             nodeActor.setSize(150, 150);
@@ -136,7 +186,12 @@ class LinkListAdapter extends AlgorithmAdapter {
         });
         await((BeforeWaitCallback)() -> {
             sDecodingThreadHandler.sendMessage(
-                    sDecodingThreadHandler.obtainMessage(ADD_TO_TAIL, 15, -1));
+                    sDecodingThreadHandler.obtainMessage(ADD_TO_N, 15, -1));
+        });
+
+        await((BeforeWaitCallback)() -> {
+            sDecodingThreadHandler.sendMessage(
+                    sDecodingThreadHandler.obtainMessage(DONE, 15, -1));
         });
     }
 }
