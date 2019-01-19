afaa2920f118864f56a1f313b26c2ce53f8880d0
Danny.Jiang <ext.danny.jiang@ef.com>
2019-01-19 12:52:54 +0800

    replace tutorial url

--- android/src/com/danny_jiang/algorithm/data_structure/array/ArrayTutorialFragment.java
+++ android/src/com/danny_jiang/algorithm/data_structure/array/ArrayTutorialFragment.java
@@ -7,7 +7,7 @@ public class ArrayTutorialFragment extends TutorialFragment {
     @Override
     public void loadUrl() {
         //访问Android assets文件夹内的
-        String url = "https://blog.csdn.net/zxm317122667/article/details/83781343";
+        String url = "https://mp.weixin.qq.com/s/YcOvI-W4-5PKMqYrhA3ymw";
         webView.loadUrl(url);
     }
 }
