9500a612de63e34eca61be8ff985387ad85d6a9d
Danny.Jiang <ext.danny.jiang@ef.com>
2019-02-14 22:00:52 +0800

    change url for queue

--- android/src/com/danny_jiang/algorithm/data_structure/queue/QueueTutorialFragment.java
+++ android/src/com/danny_jiang/algorithm/data_structure/queue/QueueTutorialFragment.java
@@ -5,6 +5,6 @@ import com.danny_jiang.algorithm.common.TutorialFragment;
 public class QueueTutorialFragment extends TutorialFragment {
     @Override
     public void loadUrl() {
-        webView.loadUrl("https://blog.csdn.net/zxm317122667/article/details/84705046");
+        webView.loadUrl("https://mp.weixin.qq.com/s/ihYINaVUEFHxpXJ-3J3EyA");
     }
 }
