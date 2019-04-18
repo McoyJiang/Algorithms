304f69797ad11ec35c3d9d252289f4d7c447150b
Danny.Jiang <ext.danny.jiang@ef.com>
2019-04-18 16:03:50 +0800

    modify tree introduction

--- android/src/com/danny_jiang/algorithm/data_structure/tree/TreeTutorialFragment.java
+++ android/src/com/danny_jiang/algorithm/data_structure/tree/TreeTutorialFragment.java
@@ -5,7 +5,9 @@ import com.danny_jiang.algorithm.common.TutorialFragment;
 public class TreeTutorialFragment extends TutorialFragment {
     @Override
     public void loadUrl() {
-        webView.loadUrl("https://blog.csdn.net/zxm317122667/" +
-                "article/details/85003626");
+        webView.loadUrl("https://mp.weixin.qq.com/s?__biz=MzU3Mjc5NjAzMw==" +
+                "&mid=2247483840&idx=1&sn=11aa78388b4694135152b3e46a20d4a9&chksm" +
+                "=fcca3c2ecbbdb53842139d794777f57b9060bf39ddc66b30e5c2dba08e8e9a30dbbc6ff5a705&token=" +
+                "1333398132&lang=zh_CN#rd");
     }
 }
